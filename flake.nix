6761b98f2dee237e21fb798662232fa83f836d36
woxQAQ <woxqaq@gmail.com>
2026-02-07 22:18:17 +0800

    feat: introduce nix dev shell to avoid link errors

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313
+            uv
+            # System libraries needed for NumPy and other scientific packages
+            zlib
+            gcc
+            stdenv.cc.cc.lib
+          ];
+
+          # Ensure libraries are findable at runtime
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+          ];
+          LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+          ];
+
+          shellHook = ''
+            echo "Python development environment loaded"
+            echo "Python: $(python --version)"
+            echo "UV: $(uv --version)"
+          '';
+        };
+      });
+}
