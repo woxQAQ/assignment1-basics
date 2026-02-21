48d0aef614a808202c7a05f7ea1ed3ee318f17e6
woxQAQ <woxqaq@gmail.com>
2026-02-21 16:15:39 +0800

    chore: tidy torch.Tensor

--- src/transformer.py
+++ src/transformer.py
@@ -2,6 +2,7 @@ from einops import einsum
 from jaxtyping import Float, Bool, Int
 import math
 import torch
+from torch import device, dtype, Tensor, sigmoid
 
 
 class Linear(torch.nn.Module):
@@ -9,8 +10,8 @@ class Linear(torch.nn.Module):
         self,
         in_features: int,
         out_features: int,
-        device: torch.device | None = None,
-        dtype: torch.dtype | None = None,
+        device: device | None = None,
+        dtype: dtype | None = None,
     ):
         super().__init__()
         self.in_features = in_features
@@ -29,8 +30,8 @@ class Linear(torch.nn.Module):
         )
 
     def forward(
-        self, x: Float[torch.Tensor, "... d_in"]
-    ) -> Float[torch.Tensor, "... d_out"]:
+        self, x: Float[Tensor, "... d_in"]
+    ) -> Float[Tensor, "... d_out"]:
         return einsum(
             x,
             self.weight,
@@ -44,8 +45,8 @@ class Embedding(torch.nn.Module):
         self,
         num_embeddings: int,
         embedding_dim: int,
-        device: torch.device | None = None,
-        dtype: torch.dtype | None = None,
+        device: device | None = None,
+        dtype: dtype | None = None,
     ):
         super().__init__()
         self.num_embeddings = num_embeddings
@@ -59,7 +60,7 @@ class Embedding(torch.nn.Module):
             self.weight, mean=0.0, std=1.0, a=-3.0, b=3.0
         )
 
-    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+    def forward(self, token_ids: Tensor) -> Tensor:
         return self.weight[token_ids]
 
 
@@ -68,8 +69,8 @@ class RMSNorm(torch.nn.Module):
         self,
         d_model: int,
         eps: float = 1e-5,
-        device: torch.device | None = None,
-        dtype: torch.dtype | None = None,
+        device: device | None = None,
+        dtype: dtype | None = None,
     ):
         super().__init__()
         self.eps = eps
@@ -79,7 +80,7 @@ class RMSNorm(torch.nn.Module):
         torch.nn.init.ones_(self.weight)
         pass
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         in_dtype = x.dtype
         x = x.to(torch.float32)
         rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)
@@ -89,8 +90,8 @@ class RMSNorm(torch.nn.Module):
 
 
 class SiLU(torch.nn.Module):
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x * torch.sigmoid(x)
+    def forward(self, x: Tensor) -> Tensor:
+        return x * sigmoid(x)
 
 
 class SwiGLU(torch.nn.Module):
@@ -108,8 +109,8 @@ class SwiGLU(torch.nn.Module):
         self.silu = SiLU()
 
     def forward(
-        self, x: Float[torch.Tensor, "... d_model"]
-    ) -> Float[torch.Tensor, "... d_model"]:
+        self, x: Float[Tensor, "... d_model"]
+    ) -> Float[Tensor, "... d_model"]:
         return self.w2(self.silu(self.w1(x)) * self.w3(x))
 
 
@@ -119,7 +120,7 @@ class RoPE(torch.nn.Module):
         theta: float,
         d_k: int,
         max_seq_len: int,
-        device: torch.device | None = None,
+        device: device | None = None,
     ):
         super().__init__()
         pos = torch.arange(max_seq_len)
@@ -129,14 +130,12 @@ class RoPE(torch.nn.Module):
         self.register_buffer("cos_cache", theta_ik.cos(), persistent=False)
         self.register_buffer("sin_cache", theta_ik.sin(), persistent=False)
 
-    def _rotate_half(self, x: torch.Tensor) -> torch.Tensor:
+    def _rotate_half(self, x: Tensor) -> Tensor:
         x0 = x[..., ::2]
         x1 = x[..., 1::2]
         return torch.stack((-x1, x0), dim=-1).flatten(-2)
 
-    def forward(
-        self, x: torch.Tensor, token_positions: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, x: Tensor, token_positions: Tensor) -> Tensor:
         sin = self.sin_cache[token_positions].to(x.device)
         cos = self.cos_cache[token_positions].to(x.device)
         xdtype = x.dtype
@@ -148,7 +147,7 @@ class RoPE(torch.nn.Module):
 
 
 # softmax(v,i) = exp(v_i) / sum(exp(v_j),j=1...n)
-def softmax(in_features: torch.Tensor, dim: int = -1) -> torch.Tensor:
+def softmax(in_features: Tensor, dim: int = -1) -> Tensor:
     # the exp must get a large value that cause overflow(be inf)
     # cause that inf/inf = NaN
     # notice that softmax is shift invariance, so we can add a shift on input
@@ -161,11 +160,11 @@ def softmax(in_features: torch.Tensor, dim: int = -1) -> torch.Tensor:
 
 
 def attention(
-    query: Float[torch.Tensor, " ... queries d_k"],
-    key: Float[torch.Tensor, " ... keys d_k"],
-    value: Float[torch.Tensor, " ... values d_v"],
-    mask: Bool[torch.Tensor, " ... queries keys"] | None = None,
-) -> Float[torch.Tensor, " ... queries d_v"]:
+    query: Float[Tensor, " ... queries d_k"],
+    key: Float[Tensor, " ... keys d_k"],
+    value: Float[Tensor, " ... values d_v"],
+    mask: Bool[Tensor, " ... queries keys"] | None = None,
+) -> Float[Tensor, " ... queries d_v"]:
     d_k = query.shape[-1]
     attn_out = einsum(
         query, key, " ... queries d_k, ... keys d_k -> ... queries keys"
@@ -183,7 +182,7 @@ def attention(
     return out
 
 
-def attn_mask(Q: torch.Tensor, K: torch.Tensor):
+def attn_mask(Q: Tensor, K: Tensor):
     # Q: [B,H,t_q,D]
     # K: [B,H,t_k,D]
     # we should get a upper triangle, with shape
@@ -214,7 +213,7 @@ class MutiHeadSelfAttention(torch.nn.Module):
         self,
         d_model: int,
         num_heads: int,
-        token_positions: Int[torch.Tensor, " ... seq_len"] | None = None,
+        token_positions: Int[Tensor, " ... seq_len"] | None = None,
         max_seq_len: int | None = None,
         theta: float | None = None,
     ):
@@ -230,20 +229,20 @@ class MutiHeadSelfAttention(torch.nn.Module):
         self.v_proj: Linear = Linear(d_model, d_model)
         self.output_proj: Linear = Linear(d_model, d_model)
         self.rope: RoPE | None = None
-        self.token_position: torch.Tensor | None = token_positions
+        self.token_position: Tensor | None = token_positions
         if theta is not None and max_seq_len is not None:
             self.rope = RoPE(theta, self.d_k, max_seq_len)
 
     # TODO: merge Q,K,V to a single weight proj, so that we can reduce
     # compute amount
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         B, T, _ = x.shape
 
-        def _split_head(_x: torch.Tensor) -> torch.Tensor:
+        def _split_head(_x: Tensor) -> Tensor:
             return _x.view(B, T, self.num_heads, self.d_k).transpose(1, 2)
 
         # reverse of _split_head
-        def _merge_head(_x: torch.Tensor) -> torch.Tensor:
+        def _merge_head(_x: Tensor) -> Tensor:
             return _x.transpose(1, 2).contiguous().view(B, T, self.d_model)
 
         Q = _split_head(self.q_proj.forward(x))
@@ -280,7 +279,11 @@ class TransformerBlock(torch.nn.Module):
         )
         self.ffn = SwiGLU(d_model, d_ff)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        h: torch.Tensor = self.attn(self.ln1(x)) + x
-        y = h + self.ffn(self.ln2(h))
+    def forward(self, x: Tensor) -> Tensor:
+        h: Tensor = self.attn(self.ln1(x)) + x
+        y: Tensor = h + self.ffn(self.ln2(h))
         return y
+
+
+class Model:
+    pass
