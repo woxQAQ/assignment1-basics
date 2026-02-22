421b1387171f48024ae248aabbc794dfc7511062
woxQAQ <woxqaq@gmail.com>
2026-02-23 03:25:05 +0800

    transformer: learn rate cosine scheduler

--- src/transformer.py
+++ src/transformer.py
@@ -463,3 +463,25 @@ class AdamW(torch.optim.Optimizer):
                 if weight_decay > 0:
                     p.add_(p, alpha=-lr * weight_decay)
         return loss
+
+
+def lr_cosine_schedule(
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosine_cycle_iters: int,
+) -> float:
+    if it < warmup_iters:
+        return (it / warmup_iters) * max_learning_rate
+    elif warmup_iters <= it <= cosine_cycle_iters:
+        pi_param = (
+            (it - warmup_iters)
+            / (cosine_cycle_iters - warmup_iters)
+            * math.pi
+        )
+        return min_learning_rate + 0.5 * (1 + math.cos(pi_param)) * (
+            max_learning_rate - min_learning_rate
+        )
+    else:
+        return min_learning_rate
--- tests/adapters.py
+++ tests/adapters.py
@@ -589,7 +589,15 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+    from src.transformer import lr_cosine_schedule
+
+    return lr_cosine_schedule(
+        it,
+        max_learning_rate,
+        min_learning_rate,
+        warmup_iters,
+        cosine_cycle_iters,
+    )
 
 
 def run_save_checkpoint(
